176e3d4616344070bd448065425ad024adf9a9e1
laoliu <laoliu@foxemali.com>
2020-06-09 10:41:34 +0800

    404页面完成

--- src/components/content/con_404.js
+++ src/components/content/con_404.js
@@ -6,10 +6,16 @@ const Con404 = () => {
     return (
         <div className="content_reset">
             <div className="con_404">
-                <img src="../../assets/404.jpg" alt="404"/>
-                <Link to="/">
-                    <div className="con_404_tips">回到首页</div>
-                </Link>
+                <div className="con_404_img"/>
+                <div className="con_404_tips">不好意思，暂无此页面，你可以</div>
+                <div className="con_404_tips">
+                    <Link to="/">
+                        <span>返回首页</span>
+                    </Link>
+                    <Link to="/web">
+                        <span>返回文章页</span>
+                    </Link>
+                </div>
             </div>
         </div>
     );
